ccc33a0ac7ec57ea726aa83c7a539fd2072e2c8c
Xostron <aat9044107480@gmail.com>
2023-01-25 01:19:51 +0300

    anim mission

--- landing/script/index.js
+++ landing/script/index.js
@@ -20,11 +20,38 @@ inputs.forEach((input) => {
 });
 
 // *************анимация page mission
-// *************анимация cookie
-const cookie = document.querySelector('.cookie')
-setTimeout(()=>{cookie.classList.remove('vis')},500)
-function hiddenCookie(){
-    console.log('hide')
-    cookie.classList.add('vis')
-}
+const image = document.querySelector(".mission_left__content");
+const missionText = document.querySelector(".mission_right__content");
 
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    console.log("TOTAL ========\n", entry);
+    if (
+      entry.isIntersecting ||
+      (entry.isVisible && entry.intersectionRatio >= 0.9)
+    ) {
+      console.log("add style");
+      missionText.classList.add("mission_right__content_anim");
+      image.classList.add("mission_left__content_anim");
+    } else {
+      console.log("remove style");
+      missionText.classList.remove("mission_right__content_anim");
+      image.classList.remove("mission_left__content_anim");
+    }
+  },
+  {
+    rootMargin: "0px 0px 0px 0px",
+    threshold: [.8],
+  }
+);
+const mission = document.querySelector(".mission");
+observer.observe(mission);
+// *************анимация cookie
+const cookie = document.querySelector(".cookie");
+setTimeout(() => {
+  cookie.classList.remove("vis");
+}, 500);
+function hiddenCookie() {
+  console.log("hide");
+  cookie.classList.add("vis");
+}
